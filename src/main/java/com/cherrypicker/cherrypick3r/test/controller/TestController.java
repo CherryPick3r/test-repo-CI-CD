11eef969a1ff7b926d8e888b9f24c5d7695e2bb5
ebang <dskhe8@gmail.com>
2023-05-25 14:31:08 +0900

    fix: application yaml for AWS parameter store

--- src/main/java/com/cherrypicker/cherrypick3r/test/controller/TestController.java
+++ src/main/java/com/cherrypicker/cherrypick3r/test/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.cherrypicker.cherrypick3r.test.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
+    @Value("${spring.datasource.url}")
+    private String data;
     @GetMapping("/")
     public String testReturn(){
         String t = "${TEST}";
         return t;
     }
+
+    @GetMapping("/2")
+    public String test(){
+        return data;
+    }
+
 }
